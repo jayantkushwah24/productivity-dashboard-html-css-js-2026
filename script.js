b3e79c167026ccf390e2d8867cad2b4eec15a49d
Jayant Kushwah <jayantkushwah8@gmail.com>
2026-07-08 10:11:38 +0530

    html js of dailyPlanner

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ export function getTodoListTasks() {
   const result = JSON.parse(localStorage.getItem("todo-list-tasks")) || [];
   return result;
 }
+
+export function getPlannerTableTask() {
+  let plannerTableTask =
+    JSON.parse(localStorage.getItem("planner-task-table")) || [];
+  return plannerTableTask;
+}
